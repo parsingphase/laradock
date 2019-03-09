e74169b076c9c6f6674a08aec96202d495188b7b
Richard George <richard@phase.org>
2019-03-09 12:19:55 +0000

    Add local defaults and startup script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#get script dir
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ ! -f "${DIR}/.env" ]]; then
+    echo "No .env file, copying env-example"
+    cp "${DIR}/env-example" "${DIR}/.env"
+fi
+
+docker-compose up -d nginx php-fpm workspace
+
+echo
+echo "Run 'docker-compose exec workspace bash' to enter workspace"
